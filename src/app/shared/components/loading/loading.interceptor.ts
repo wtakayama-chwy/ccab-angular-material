47cd11314b21c8e4824ad64ed776f09c7f05b9ca
William S. Takayama <william-sobreira@Hotmail.com>
2020-06-17 03:51:50 -0300

    loading interceptor

---
+++ src/app/shared/components/loading/loading.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { LoadingService } from './loading.service';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpSentEvent, HttpHeaderResponse, HttpProgressEvent, HttpResponse, HttpUserEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class LoadingIntercptor implements HttpInterceptor {
+
+    constructor(
+        private loadingService: LoadingService
+    ) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler):
+        Observable<HttpSentEvent |
+            HttpHeaderResponse |
+            HttpProgressEvent |
+            HttpResponse<any> |
+            HttpUserEvent<any>> {
+        return next
+            .handle(req)
+            .pipe(tap(event => {
+                if (event instanceof HttpResponse) {
+                    this.loadingService.stop();
+                } else {
+                    this.loadingService.start();
+                }
+            }))
+    }
+
+}
